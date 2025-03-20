882c4690a3774d45dcb7beb0fc3c40dbd8645e8c
Daniel <danielpg2020md@gmail.com>
2025-03-20 18:25:39 -0500

    fix: ajustes en archivos generados

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,13 +5,8 @@
 import FlutterMacOS
 import Foundation
 
-<<<<<<< HEAD
-
-func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-=======
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
->>>>>>> 11cad6d (feat: implementación de diseño y gestión de enlaces)
 }
